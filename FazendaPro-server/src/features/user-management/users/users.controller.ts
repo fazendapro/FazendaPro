ab67c0a72a184707f78cdba14cb03e97c6ccd143
guusdias <gustavohd34@gmail.com>
2025-03-20 14:52:51 -0300

    fix: bcrypt to js

--- FazendaPro-server/src/features/user-management/users/users.controller.ts
+++ FazendaPro-server/src/features/user-management/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Delete, Get, Param, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './users.entity';
+import { CreateUserDto } from './dto/create-user-dto';
 
 @Controller('users')
 export class UsersController {
@@ -17,8 +18,8 @@ export class UsersController {
   }
 
   @Post()
-  create(@Body() user: User) {
-    return this.usersService.create(user);
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
   }
 
   @Delete(':id')
--- FazendaPro-server/src/features/user-management/users/users.service.ts
+++ FazendaPro-server/src/features/user-management/users/users.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './users.entity';
-
+import { CreateUserDto } from './dto/create-user-dto';
+import * as bcrypt from 'bcryptjs';
 @Injectable()
 export class UsersService {
   constructor(
@@ -22,8 +23,24 @@ export class UsersService {
     return this.usersRepository.findOneBy({ firstName: username });
   }
 
-  async create(user: User): Promise<User> {
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
+
+    const user = this.usersRepository.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
     return this.usersRepository.save(user);
+  }
+
+  async validateUser(email: string, password: string): Promise<User | null> {
+    const user = await this.usersRepository.findOne({ where: { email } });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user;
+    }
+    return null;
   }
 
   async remove(id: number): Promise<void> {
