a857304c5cc5b0ea900fbb244dfd2b254f80b581
guusdias <gustavohd34@gmail.com>
2025-04-12 17:31:10 -0300

    chore: remove notifications service

--- FazendaPro-notifications/lib/farmnotifications/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Farmnotifications.Mailer do
-  use Swoosh.Mailer, otp_app: :farmnotifications
-end
--- FazendaPro-notifications/lib/farmnotifications_web/controllers/page_controller.ex
+++
@@ -1,8 +0,0 @@
-defmodule FarmNotificationsWeb.PageController do
-  use FarmNotificationsWeb.WhatsApp
-
-  def create(conn, %{"event" => "novo_gado", "data" => data}) do
-    FarmNotificationsWeb.Endpoint.broadcast("gado:notifications", "novo_gado", data)
-    json(conn, %{status: "ok"})
-  end
-end
