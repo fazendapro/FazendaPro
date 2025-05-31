e4e5b0164e9a6da4260019b04939893d9b066c62
guusdias <gustavohd34@gmail.com>
2025-05-31 16:24:16 -0300

    chore: moving services from main to service branch

--- FazendaPro-presentation/src/pages/Login/styles.ts
+++
@@ -1,7 +0,0 @@
-export const baseStyle : React.CSSProperties = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  padding: window.innerWidth > 768 ? '150px' : '0px'
-}
