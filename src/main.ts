5a5541f21cb3425eecac80ca39c02fefebe1b05b
guusdias <gustavohd34@gmail.com>
2025-05-31 17:30:23 -0300

    chore: configuring cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
+  const corsOptions: CorsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  };
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: true,
+    cors: corsOptions,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
