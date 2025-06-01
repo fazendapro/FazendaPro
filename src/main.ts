488e6bc29023d3a644e8a57cdb6e57739a1bb824
guusdias <gustavohd34@gmail.com>
2025-05-31 23:20:01 -0300

    fix: cors origin on stage

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
+  console.log(process.env.FRONTEND_URL);
   const corsOptions: CorsOptions = {
     origin: process.env.FRONTEND_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
@@ -14,6 +18,8 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: corsOptions,
   });
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = parseInt(process.env.PORT || '3000', 10);
+  await app.listen(port);
 }
 bootstrap();
