3316359469af94e74881290b3ba8f8c935a7cba3
guusdias <gustavohd34@gmail.com>
2025-06-28 18:40:57 -0300

    refactor: chaging name of component and box shadow

--- src/components/DashobardInfoCard/dashboard-info-card.tsx
+++ src/components/DashobardInfoCard/dashboard-info-card.tsx
@@ -1,6 +1,6 @@
 import { Card } from "antd";
 
-export const InfoCard = ({title, value, icon, isLast}: {title: string, value: string, icon: React.ReactNode, isLast: boolean}) => {
+export const DashboardInfoCard = ({title, value, icon, isLast}: {title: string, value: string, icon: React.ReactNode, isLast: boolean}) => {
   return (
       <Card style={{ textAlign: 'center', borderRadius: 0, border:'none', borderRight: isLast ? 'none' : '1px solid rgba(0,0,0,0.1)'}}>
           {icon}
