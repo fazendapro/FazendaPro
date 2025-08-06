19eb9426e3d65f631e5128ebfb1b8e3f5274e2af
guusdias <gustavohd34@gmail.com>
2025-08-06 20:59:33 -0300

    fix: undefined url

--- src/components/services/axios/base.ts
+++ src/components/services/axios/base.ts
@@ -2,6 +2,10 @@ import axios, { AxiosInstance } from 'axios'
 
 export function baseAxios(baseUrl: string): AxiosInstance {
   const apiUrl = import.meta.env.VITE_API_URL
+  
+  if (!apiUrl) {
+    throw new Error('VITE_API_URL não está definida. Verifique as variáveis de ambiente.')
+  }
 
   return axios.create({
     baseURL: `${apiUrl}/${baseUrl}` 
