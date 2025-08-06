62c24e8a2c98ff417972b3d4f85693be71c61579
guusdias <gustavohd34@gmail.com>
2025-08-06 20:47:37 -0300

    fix: request

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    },
+    define: {
+      'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
     }
   }
 })
